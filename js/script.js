ecc1c217c416edc859f7f35da14d3ad706cc8306
Riccarpa <riccardopagni99@gmail.com>
2021-07-06 09:51:37 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -4,27 +4,30 @@ Consegna:
 Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-
-
 */
-
-//link html
-
-stamp = document.getElementById('stamp');
-
-//array
-
-var numbersList = [];
-var message = 'fizz'
-    //vars
-
-
 
 //stampa numeri 1-100
 
+var listItems = '';
+
 for (i = 1; i <= 100; i++) {
-    numbersList.push(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+        listItems += '<li>fizzbuzz </li>';
+    } else if (i % 3 === 0) {
+        console.log('fizz');
+        listItems += '<li>fizz </li>';
+    } else if (i % 5 === 0) {
+        console.log('buzz');
+        listItems += '<li>buzz</li>';
+    } else {
+        console.log(i);
+        listItems += '<li>' + i + '</li>';
+    }
 
 }
 
-stamp.innerHTML = numbersList;
+
+//stamp
+stamp = document.getElementById('stamp');
+stamp.innerHTML = listItems;
